caf8e6b3a37ce381cb0a1bc0e37dfa48806df66d
lliu-student <2522685828@qq.com>
2023-07-02 23:02:12 +0800

    feat:db-prisma

---
+++ tests/db.test.ts
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+
+describe("db set", () => {
+  const prisma = new PrismaClient();
+
+  async function main() {
+    // ... you will write your Prisma Client queries here
+    // await prisma.user.create({
+    //   data: {
+    //     name: "Alice",
+    //     email: "alice@prisma.io",
+    //     posts: {
+    //       create: { title: "Hello World" },
+    //     },
+    //     profile: {
+    //       create: { bio: "I like turtles" },
+    //     },
+    //   },
+    // });
+    const allUsers = await prisma.user.findMany({
+      include: {
+        posts: true,
+        profile: true,
+      },
+    });
+    console.log(allUsers);
+    const post = await prisma.post.update({
+      where: { id: 1 },
+      data: { published: true },
+    });
+    console.log(post);
+    return post;
+  }
+
+  it("should sum two numbers", async () => {
+    expect(await main()).toStrictEqual([]);
+  });
+});
